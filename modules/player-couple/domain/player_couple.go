75d2d2bf187cfc81a1f209b65836e80eae309855
paguerre3 <camila.milagros.aguerre@gmail.com>
2024-09-18 13:59:43 -0300

    todo

--- modules/player-couple/domain/player_couple.go
+++ modules/player-couple/domain/player_couple.go
@@ -1,9 +1,12 @@
 package domain
 
+import "fmt"
+
 type Player struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-	Age  int    `json:"age"`
+	ID                   string  `json:"id"`
+	SocialSecurityNumber *string `json:"ssn,omitempty"`
+	Name                 string  `json:"name"`
+	Age                  *int    `json:"age,omitempty"`
 }
 
 type PlayerCouple struct {
@@ -13,15 +16,24 @@ type PlayerCouple struct {
 	Ranking int    `json:"ranking"`
 }
 
-func NewValidPlayer(id string, name string, age int) *Player {
+func NewPlayer(socialSecurityNumber *string, name string, age *int) (*Player, error) {
+	if socialSecurityNumber != nil && len(*socialSecurityNumber) < 8 {
+		return nil, fmt.Errorf("invalid social security number: %s", *socialSecurityNumber)
+	}
+	if len(name) < 2 {
+		return nil, fmt.Errorf("invalid name: %s", name)
+	}
+	if age != nil && (*age < 3 || *age > 100) {
+		return nil, fmt.Errorf("invalid age: %d", *age)
+	}
 	return &Player{
-		ID:   id,
+		ID:   "", // TODO: generate ID
 		Name: name,
 		Age:  age,
-	}
+	}, nil
 }
 
-func NewValidPlayerCouple(id string, player1 Player, player2 Player, ranking int) *PlayerCouple {
+func NewPlayerCouple(id string, player1 Player, player2 Player, ranking int) *PlayerCouple {
 	return &PlayerCouple{
 		ID:      id,
 		Player1: player1,
