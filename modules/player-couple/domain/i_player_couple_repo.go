329a972dfb73b36d0a2bcc83215a6b3be7628011
paguerre3 <camila.milagros.aguerre@gmail.com>
2024-09-23 18:53:47 -0300

    players couple imnterface

---
+++ modules/player-couple/domain/i_player_couple_repo.go
@@ -0,0 +1,19 @@
+package domain
+
+// interface to be used by infrastructure layer:
+type PlayerRepository interface {
+	Save(player Player) error
+	FindByID(id string) (Player, error)
+	FindByEmail(email string) (Player, error)
+	FindByLastName(lastName string) ([]Player, error)
+	Update(player Player) error
+	Delete(id string) error
+}
+
+type PlayerCoupleRepository interface {
+	Save(playerCouple PlayerCouple) error
+	FindByID(id string) (PlayerCouple, error)
+	FindByPrefixes(lastNamePlayer1, lastNamePlayer2 string) ([]PlayerCouple, error)
+	Update(playerCouple PlayerCouple) error
+	Delete(id string) error
+}
