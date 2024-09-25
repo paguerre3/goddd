12dfe77a2e68b460504fae041b0c8647ca77c7b3
paguerre3 <camila.milagros.aguerre@gmail.com>
2024-09-25 15:39:43 -0300

    almost all find use cases

--- modules/player-couple/application/find_player_use_case_test.go
+++ modules/player-couple/application/find_player_use_case_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestFindPlayerByIDUseCase(t *testing.T) {
-	t.Run("Valid player ID found", func(t *testing.T) {
+	t.Run("Valid ID player found", func(t *testing.T) {
 		// Arrange
 		idGen := &MockIDGenerator{}
 		repo := &MockPlayerRepository{}
@@ -59,7 +59,7 @@ func TestFindPlayerByIDUseCase(t *testing.T) {
 		assert.Equal(t, domain.Player{}, player)
 	})
 
-	t.Run("Error in repository", func(t *testing.T) {
+	t.Run("Error in repository finding by ID", func(t *testing.T) {
 		// Arrange
 		idGen := &MockIDGenerator{}
 		repo := &MockPlayerRepository{}
@@ -75,5 +75,132 @@ func TestFindPlayerByIDUseCase(t *testing.T) {
 		assert.Error(t, err)
 		assert.Equal(t, expectedErr, err)
 		assert.Equal(t, domain.Player{}, player)
+	})
+}
+
+func TestFindPlayerByEmailUseCase(t *testing.T) {
+	t.Run("Valid email player found", func(t *testing.T) {
+		// Arrange
+		idGen := &MockIDGenerator{}
+		repo := &MockPlayerRepository{}
+		service := NewFindPlayerUseCase(repo, idGen)
+		email := "test@example.com"
+		foundPlayer := domain.Player{Email: email}
+		repo.On("FindByEmail", email).Return(foundPlayer, nil)
+
+		// Act
+		player, err := service.FindPlayerByEmailUseCase(email)
+
+		// Assert
+		assert.NoError(t, err)
+		assert.Equal(t, foundPlayer, player)
+	})
+
+	t.Run("Valid email not player found", func(t *testing.T) {
+		// Arrange
+		idGen := &MockIDGenerator{}
+		repo := &MockPlayerRepository{}
+		service := NewFindPlayerUseCase(repo, idGen)
+		email := "test@example.com"
+		repo.On("FindByEmail", email).Return(domain.Player{}, nil)
+
+		// Act
+		player, err := service.FindPlayerByEmailUseCase(email)
+
+		// Assert
+		assert.NoError(t, err)
+		assert.Equal(t, domain.Player{}, player)
+	})
+
+	t.Run("Invalid email", func(t *testing.T) {
+		// Arrange
+		idGen := &MockIDGenerator{}
+		repo := &MockPlayerRepository{}
+		service := NewFindPlayerUseCase(repo, idGen)
+		email := "invalid-email"
+		expectedErr := domain.ValidateEmail(email)
+
+		// Act
+		player, err := service.FindPlayerByEmailUseCase(email)
+
+		// Assert
+		assert.Error(t, err)
+		assert.Equal(t, expectedErr, err)
+		assert.Equal(t, domain.Player{}, player)
+	})
+
+	t.Run("Error in repository finding by email", func(t *testing.T) {
+		// Arrange
+		idGen := &MockIDGenerator{}
+		repo := &MockPlayerRepository{}
+		service := NewFindPlayerUseCase(repo, idGen)
+		email := "test@example.com"
+		repoErr := errors.New("repository error")
+		repo.On("FindByEmail", email).Return(domain.Player{}, repoErr)
+
+		// Act
+		player, err := service.FindPlayerByEmailUseCase(email)
+
+		// Assert
+		assert.Error(t, err)
+		assert.Equal(t, repoErr, err)
+		assert.Equal(t, domain.Player{}, player)
+	})
+}
+
+func TestFindPlayersByLastNameUseCase(t *testing.T) {
+	t.Run("Valid last name players found", func(t *testing.T) {
+		// Arrange
+		repo := &MockPlayerRepository{}
+		service := NewFindPlayerUseCase(repo, &MockIDGenerator{})
+		lastName := "Doe"
+		expectedPlayers := []domain.Player{{ID: "1", LastName: "Doe"}}
+
+		repo.On("FindByLastName", lastName).Return(expectedPlayers, nil)
+
+		// Act
+		players, err := service.FindPlayersByLastNameUseCase(lastName)
+
+		// Assert
+		assert.NoError(t, err)
+		assert.Equal(t, expectedPlayers, players)
+	})
+
+	t.Run("Invalid last name players not browsed", func(t *testing.T) {
+		// Arrange
+		repo := &MockPlayerRepository{}
+		service := NewFindPlayerUseCase(repo, &MockIDGenerator{})
+		lastName := ""
+		expectedErr := domain.ValidateLastName(lastName)
+
+		// Act
+		players, err := service.FindPlayersByLastNameUseCase(lastName)
+
+		// Assert
+		assert.Error(t, err)
+		assert.Equal(t, expectedErr, err)
+		assert.Nil(t, players)
+	})
+
+	t.Run("Error in repository finding by last name", func(t *testing.T) {
+		// Arrange
+		repo := &MockPlayerRepository{}
+		service := NewFindPlayerUseCase(repo, &MockIDGenerator{})
+		lastName := "Doe"
+		expectedErr := errors.New("repo error")
+
+		// TODO: fix this
+		//repo.On("FindByLastName", lastName).Return(nil, expectedErr)
+		repo.On("FindByLastName", lastName).Return([]domain.Player{}, expectedErr)
+
+		// Act
+		players, err := service.FindPlayersByLastNameUseCase(lastName)
+
+		// Assert
+		assert.Error(t, err)
+		assert.Equal(t, expectedErr, err)
+		// TODO: fix this
+		//assert.Nil(t, players)
+		assert.Equal(t, []domain.Player{}, players)
 	})
 }
