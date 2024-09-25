160f7e7cbc728580392c33db571eace49ee10813
paguerre3 <camila.milagros.aguerre@gmail.com>
2024-09-24 23:44:10 -0300

    renames

--- modules/player-couple/application/player_couple_service.go
+++ modules/player-couple/application/player_couple_service.go
@@ -2,19 +2,10 @@ package application
 
 import "github.com/paguerre3/goddd/modules/player-couple/domain"
 
-type PlayerService struct {
-	playerRepo domain.PlayerRepository
-	idGen      domain.IDGenerator
-}
-
 type PlayerCoupleService struct {
 	playerRepo       domain.PlayerRepository
 	playerCoupleRepo domain.PlayerCoupleRepository
 	idGen            domain.IDGenerator
-}
-
-func NewPlayerService(playerRepository domain.PlayerRepository, idGenerator domain.IDGenerator) *PlayerService {
-	return &PlayerService{playerRepo: playerRepository, idGen: idGenerator}
 }
 
 func NewPlayerCoupleService(playerRepository domain.PlayerRepository, playerCoupleRepository domain.PlayerCoupleRepository,
